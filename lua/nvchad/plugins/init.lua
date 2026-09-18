ef5f4c7e83e23c3ba80adf8ce429aa9cae3c1262
ilovemikael <itsmeguys2247@gmail.com>
2026-09-17 23:49:38 -0400

    trolololool

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -107,35 +107,6 @@ return {
     end,
   },
 
-  -- load luasnips + cmp related in insert mode only
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require "nvchad.configs.luasnip"
-        end,
-      },
-      -- cmp sources plugins
-      {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-       "https://codeberg.org/FelipeLema/cmp-async-path.git",
-      },
-    },
-    opts = function()
-      return require "nvchad.configs.cmp"
-    end,
-  },
-
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
